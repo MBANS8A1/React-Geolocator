89f570fe1167352cf6fbff5ec5543a5ce4955d25
MBANS8A1 <seanmbng@aol.com>
2025-11-14 16:46:17 +0000

    I created 4 state hooks for finding the position of the user (longitude and latitude using navigator.geolocation). I need to consider loading when the data is being fetched, a click count so the user knows how many times they have requested their position, an evaluation of any errors and the object that will store the longitude and latitude properties and their values.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [countClicks, setCountClicks] = useState(0);
+  const [error, setError] = useState(null);
+  const [position, setPosition] = useState({});
   return <div className="App"></div>;
 }
 
